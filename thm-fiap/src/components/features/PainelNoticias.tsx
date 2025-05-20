10b12a1077cd41c926fc6ebed0794875d02db49e
felipecoelho01 <felipecoelho12@outlook.com.br>
2025-05-19 22:57:24 -0300

    teste

--- thm-fiap/src/components/features/PainelNoticias.tsx
+++ thm-fiap/src/components/features/PainelNoticias.tsx
@@ -11,14 +11,14 @@ const PainelNoticias = () => {
       id: 1,
       title: "Fortes chuvas atingem São Paulo e alagam ruas",
       content: "Publicado em abril de 2025, este artigo da Agência Brasil relata alagamentos em diversas regiões da capital paulista devido a fortes chuvas.",
-      img: "../../../public/noticia1.jpg",
+      img: "/noticia1.jpg",
       url: "https://agenciabrasil.ebc.com.br/geral/noticia/2025-04/chuvas-fortes-atingem-sao-paulo-e-alagam-ruas",
     },
     {
       id: 2,
       title: "Temporal causa alagamentos na região metropolitana de São Paulo",
       content: "Em fevereiro de 2025, a Agência Brasil reportou alagamentos e chamados de emergência devido a um temporal na região metropolitana.",
-      img: "../../../public/noticia2.png",
+      img: "/noticia2.png",
       url: "https://www.cnnbrasil.com.br/nacional/sudeste/sp/cidade-de-sao-paulo-entra-em-estado-de-atencao-para-alagamentos-3/",
     },
     {
@@ -26,21 +26,21 @@ const PainelNoticias = () => {
       title:
         "Chuva em SP causa queda de árvores, alagamentos e falta de energia",
       content: "Reportagem do UOL de janeiro de 2025 sobre os efeitos de uma forte chuva que resultou em alagamentos e interrupções no fornecimento de energia.",
-      img: "../../../public/noticia4.png",
+      img: "/noticia4.png",
       url: "https://noticias.uol.com.br/cotidiano/ultimas-noticias/2024/12/21/chuva-causa-alagamentos-e-deixa-moradores-ilhados-em-sao-paulo-veja-video.htm",
     },
     {
       id: 4,
       title: "Por que chuvas em São Paulo têm provocado tantos estragos",
       content: "Análise da BBC publicada em março de 2025 que explora as razões pelas quais as chuvas têm causado danos significativos na cidade.",
-      img: "../../../public/noticia5.jpg",
+      img: "/noticia5.jpg",
       url: "https://www.bbc.com/portuguese/articles/c4g944q07vlo",
     },
     {
       id: 5,
       title: "Córrego transborda após temporal em São Paulo | CNN 360°",
       content: "Vídeo da CNN Brasil mostrando o transbordamento de um córrego após fortes chuvas em São Paulo.",
-      img: "../../../public/noticia6.png",
+      img: "/noticia6.png",
       url: "https://www.facebook.com/watch/?v=619334270799222",
     },
   ];
